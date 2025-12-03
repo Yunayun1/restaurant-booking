2c82a4b92c9e1f2b57dc7c0fd425074ea86589cf
vottey <namtey16082005@gmail.com>
2025-12-03 19:03:05 +0700

    update

---
+++ app/home/page.tsx
@@ -0,0 +1,8 @@
+export default function HomeScreen() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>Welcome to Home Screen</h1>
+      <p>Here you can book tables.</p>
+    </div>
+  );
+}
